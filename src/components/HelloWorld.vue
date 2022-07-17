bd12cb848722897b05b8bdd1a252e8cf251a7c80
hiroyuki12 <hiroyuki12@gmail.com>
2022-07-17 20:23:33 +0900

    Change Qiita API tags React.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,21 @@
 <template>
     <div>
-        <input v-model="userId">
         <button @click="getQiitaData()">取得開始</button>
         <div v-if="isClick">
             <table class="table table-striped">
                 <tr>
                     <th>記事タイトル</th>
                     <th>URL</th>
-                    <th>LGTM数</th>
                     <th>投稿日時</th>
                 </tr>
                 <tr v-for="(item, index) in displayQiitaDataList" :key="index">
                     <td class="text-left">{{ item.title }}</td>
                     <td ><a :href="item.url">{{ item.url }}</a></td>
-                    <td>{{ item.likes_count }}</td>
                     <td>{{ item.created_at }}</td>
                 </tr>
             </table>
             <div>
                 <h3>記事数 {{ totalArticle }}コ</h3>// h3で文字サイズ調整すな←
-                <h3>LGTM数 {{ totalLGTM }}コ</h3>
             </div>
         </div>
     </div>
@@ -40,7 +36,8 @@ export default {
     },
     methods: {
         getQiitaData: function() {
-            axios.get(`https://qiita.com/api/v2/users/${this.userId}/items?page=1&per_page=100`, {})
+            //axios.get(`https://qiita.com/api/v2/users//tatsuya_1995/items?page=1&per_page=100`, {})
+            axios.get(`https://qiita.com/api/v2/tags/React/items?page=1&per_page=20`, {})
             .then(res => {
                 let allQiitaData = [];
                 allQiitaData = res.data;
@@ -49,7 +46,7 @@ export default {
                 let totalLGTM = 0;
                 allQiitaData.forEach(function (item) {
                     displayQiitaDataList.push(item);
-                    totalLGTM += item.likes_count;
+                    //totalLGTM += item.likes_count;
                 })
                 // forEach内でthis.displayQiitaDataListへ格納できないので外でやる
                 this.displayQiitaDataList = displayQiitaDataList.sort();
