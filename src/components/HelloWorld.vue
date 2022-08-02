cab1b02c3755934dca68f1bc579dc110cc3e1d53
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-03 01:32:32 +0900

    Add tagButtonClick.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,8 +4,9 @@
         <font color="red"><b>{{error}}</b></font><br />
         <a href="https://mbp.hatenablog.com/entry/2022/07/16/213404" target="_blank" rel="noreferrer" className="QiitaApp-link">VercelでVue 3 + Viteのアプリを作成(vercel-vite-vue)</a><br />
         <a href="https://mbp.hatenablog.com/entry/2022/07/16/222139" target="_blank" rel="noreferrer" className="QiitaApp-link">Vite + VueでQiitaAPIを使って記事情報を取得して表示、無限スクロール Vercel</a><br />
-        <button @click="getQiitaData('react')">React</button>
-        <button @click="getQiitaData('vue.js')">Vue.js</button>
+        <button @click="tagButtonClick('react')">React</button>
+        <button @click="tagButtonClick('vue.js')">Vue.js</button>
+        <button @click="clearButtonClick()">Clear</button>
         {{tag}}<br />
         <button @click="pageButtonClick()">__10__</button>{{page}}
         <div v-if="isClick">
@@ -39,27 +40,41 @@ import axios from "axios";
 export default {
     data() {
         return {
-            userId: "",
+            //userId: "",
             displayQiitaDataList: "",
             totalArticle: 0,
-            totalLGTM: 0,
+            //totalLGTM: 0,
             isClick: false,
             page: 0,
             tag: "Vue.js",
             allQiitaData: [],
-            top: 0,
-            inner: 0,
-            offset: 0,
+            //top: 0,
+            //inner: 0,
+            //offset: 0,
             error: "",
             isLoading: false,
         }
     },
     methods: {
+        clearButtonClick: function() {
+          this.page = 0;
+          this.tag = "";
+          this.allQiitaData.splice(0);
+          this.displayQiitaDataList.splice(0);
+        },
+        tagButtonClick: function(tag) {
+          this.tag = tag;
+          //this.page = 0;
+          //this.allQiitaData.splice(0);
+          //this.displayQiitaDataList.splice(0);
+
+          this.getQiitaData();
+        },
         getNextPage: function() {
           window.onscroll = () => {
-            this.top = document.documentElement.scrollTop;
-            this.inner = window.innerHeight;
-            this.offset = document.documentElement.offsetHeight;
+            //this.top = document.documentElement.scrollTop;
+            //this.inner = window.innerHeight;
+            //this.offset = document.documentElement.offsetHeight;
 
             if (
               window.innerHeight + document.documentElement.scrollTop !==
@@ -68,27 +83,27 @@ export default {
               return;
             }
 
-            this.getQiitaData(this.tag);
+            this.getQiitaData();
           }
         },
-        getQiitaData: function(tag) {
-            this.tag = tag;
+        getQiitaData: function() {
             this.isLoading = true;
             this.page = this.page + 1;
-            axios.get(`https://qiita.com/api/v2/tags/${tag}/items?page=${this.page}&per_page=20`, {})
+            //axios.get(`https://qiita.com/api/v2/tags/${tag}/items?page=${this.page}&per_page=20`, {})
+            axios.get(`https://qiita.com/api/v2/tags/${this.tag}/items?page=${this.page}&per_page=20`, {})
             .then(res => {
                 let allQiitaData = [];
                 allQiitaData = this.allQiitaData.concat(res.data);
 
                 let displayQiitaDataList = [];
-                let totalLGTM = 0;
+                //let totalLGTM = 0;
                 allQiitaData.forEach(function (item) {
                     displayQiitaDataList.push(item);
                     //totalLGTM += item.likes_count;
                 })
                 // forEach内でthis.displayQiitaDataListへ格納できないので外でやる
                 this.displayQiitaDataList = displayQiitaDataList.sort();
-                this.totalLGTM = totalLGTM;
+                //this.totalLGTM = totalLGTM;
                 // total記事数を取得
                 this.totalArticle = displayQiitaDataList.length;
                 // clickによる表示の制御
