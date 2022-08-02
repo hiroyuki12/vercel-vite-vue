8a38596db5a1cc5ab173ab79648e1026b4658c85
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-02 23:28:48 +0900

    Delete getQiitaDataReact.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,6 @@
         <a href="https://mbp.hatenablog.com/entry/2022/07/16/222139" target="_blank" rel="noreferrer" className="QiitaApp-link">Vite + VueでQiitaAPIを使って記事情報を取得して表示、無限スクロール Vercel</a><br />
         <button @click="getQiitaData('react')">React</button>
         <button @click="getQiitaData('vue.js')">Vue.js</button>
-        <button @click="getQiitaDataReact()">React</button>
         {{tag}}<br />
         <button @click="pageButtonClick()">__10__</button>{{page}}
         <div v-if="isClick">
@@ -47,7 +46,6 @@ export default {
             isClick: false,
             page: 0,
             tag: "Vue.js",
-            //tag: "React",
             allQiitaData: [],
             top: 0,
             inner: 0,
@@ -97,32 +95,10 @@ export default {
                 this.isClick = true;
                 this.allQiitaData = allQiitaData;
             }).catch(err => {
-                this.error = err.message;  // Request failed with status code 403
+                //this.error = err.message;  // Request failed with status code 403
                 this.error = "Rate limit exceeded";
             })
             //this.isLoading = false;
-        },
-        getQiitaDataReact: function() {
-            this.tag = "React";
-            axios.get(`https://qiita.com/api/v2/tags/React/items?page=1&per_page=20`, {})
-            .then(res => {
-                let allQiitaData = [];
-                allQiitaData = res.data;
-
-                let displayQiitaDataList = [];
-                let totalLGTM = 0;
-                allQiitaData.forEach(function (item) {
-                    displayQiitaDataList.push(item);
-                    //totalLGTM += item.likes_count;
-                })
-                // forEach内でthis.displayQiitaDataListへ格納できないので外でやる
-                this.displayQiitaDataList = displayQiitaDataList.sort();
-                this.totalLGTM = totalLGTM;
-                // total記事数を取得
-                this.totalArticle = displayQiitaDataList.length;
-                // clickによる表示の制御
-                this.isClick = true;
-            })
         },
         pageButtonClick: function() {
             this.page = 10;
