7e351a20a401340147f7e99a5ee42e8d793e9569
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-07 17:50:31 +0900

    Update tag Nuxt.js.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
         <button @click="tagButtonClick('React')">React</button>
         <button @click="tagButtonClick('Next.js')">Next.js</button>
         <button @click="tagButtonClick('Vue.js')">Vue.js</button>
-        <button @click="tagButtonClick('Nuxt')">Nuxt.js</button>
+        <button @click="tagButtonClick('Nuxt.js')">Nuxt.js</button>
         <button @click="tagButtonClick('Swift')">Swift</button>
         <button @click="tagButtonClick('Vim')">Vim</button>
         <button @click="tagButtonClick('Azure')">Azure</button>
